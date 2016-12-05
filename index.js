e47fb8ae9b6c452697873325d62293bc71ce8338
Sam Noedel <sam.noedel@changepoint.com>
2016-12-05 15:52:24 -0800

    Add tests

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 'use strict';
 
-const uuid = require('node-uuid');
-
 const defaultOpts = {
   header: 'CorrelationID',
-  ignoreCase: true,
-  generator: uuid.v4
+  ignoreCase: true
 };
 
 exports.cid = function correlationId(options) {
   let opts = Object.assign({ }, defaultOpts, options);
 
-  function correlationId(req, res, next) {
+  return function correlationId(req, res, next) {
     req.cid = getHeader(req, opts.header, opts.ignoreCase, 'foobar');
     next();
-  }
+  };
 };
 
-function getHeader(req, header, ignoreCase, default) {
+function getHeader(req, header, ignoreCase) {
   let reqHeader = ignoreCase
     ? req.headers.find(h => h.toLowerCase() === header.toLowerCase())
     : req.headers[header];
 
-  return reqHeader || default;
+  return reqHeader || req.id;
 }
---
+++ test.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const correlationId            = require('./index'),
+  { beforeEach, describe, it } = require('mocha'),
+  expect                       = require('code').expect;
+
+describe('Correlation ID middleware', () => {
+  let callCount = 0;
+  let next;
+  let req;
+
+  beforeEach(() => {
+    next = () => callCount++;
+    req = { };
+  });
+
+  it('should call next', () => {
+    callMiddleware(req, next);
+    expect(callCount).to.equal(1);
+  });
+
+  describe('header field', () => {
+    it('should default to CorrelationID', () => {
+      req.headers = { CorrelationID: 'foobar' };
+      callMiddleware(req, next);
+      expect(req.id).to.equal('foobar');
+    });
+
+    it('should allow specification of a custom header field', () => {
+      req.headers = { Cheese: 'foobar' };
+      callMiddleware(req, next, { header: 'Cheese' });
+      expect(req.id).to.equal('Cheese');
+    });
+  });
+
+  describe('case sensitivity', () => {
+    it('should match headers with matching case', () => {
+      req.headers = { CorrelationId: 'STOP YELLING IN THE HOUSE' };
+      callMiddleware(req, next);
+      expect(req.id).to.equal('STOP YELLING IN THE HOUSE');
+    });
+
+    it('should match headers with different cases', () => {
+      req.headers = { CorrelationID: 'h4x' };
+      callMiddleware(req, next);
+      expect(req.id).to.equal('h4x');
+    });
+  });
+});
+
+function callMiddleware(req, next, opts) {
+  correlationId.cid(opts)(req, null, next);
+}
